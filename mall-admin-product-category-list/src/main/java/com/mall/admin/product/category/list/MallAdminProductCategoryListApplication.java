8dd77490a1879517d7a98982e6db079444e59e40
guandoublex <guandoublex@163.com>
2020-09-24 14:40:50 +0800

    添加商品分类、类型配置文件

---
+++ mall-admin-product-category-list/src/main/java/com/mall/admin/product/category/list/MallAdminProductCategoryListApplication.java
@@ -0,0 +1,13 @@
+package com.mall.admin.product.category.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAdminProductCategoryListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminProductCategoryListApplication.class, args);
+    }
+
+}
---
+++ mall-admin-product-category-list/src/test/java/com/mall/admin/product/category/list/MallAdminProductCategoryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.admin.product.category.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAdminProductCategoryListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
