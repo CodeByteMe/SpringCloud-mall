0f70a0f50db7c985359ef11ea8f260228328998a
chunjieboy <2119229443@qq.com>
2020-09-24 19:51:29 +0800

    修改了........

---
+++ mall-user-action/src/main/java/com/mall/user/action/MallUserActionApplication.java
@@ -0,0 +1,13 @@
+package com.mall.user.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUserActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserActionApplication.class, args);
+    }
+
+}
--- mall-user-list/src/main/java/com/mall/user/list/service/UserService.java
+++ mall-user-list/src/main/java/com/mall/user/list/service/UserService.java
@@ -1,9 +1,11 @@
 package com.mall.user.list.service;
 
+import com.github.pagehelper.PageInfo;
 import com.mall.common.pojo.AdminUser;
 
 import java.util.List;
 
 public interface UserService {
-    public List<AdminUser> selectUserListBycompanyid(String companyId, int page, int limit);
+    public PageInfo UserListBycompanyId( String companyId,int pageNum,int pageSize);
+    public String getCid(String adminId);
 }
