8a305b3800e1ee5bcf6d4409072330ea853632a5
chunjieboy <2119229443@qq.com>
2020-09-25 20:30:22 +0800

    update

---
+++ mall-menu-list/src/test/java/com/mall/menu/list/MallMenuListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.menu.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMenuListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
