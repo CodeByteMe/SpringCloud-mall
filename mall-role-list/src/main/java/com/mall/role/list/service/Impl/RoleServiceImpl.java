0b01852e38817a6be19e3d9a42ed0e17e07cb097
chunjieboy <2119229443@qq.com>
2020-09-25 14:46:59 +0800

    修改了...

--- mall-role-list/src/main/java/com/mall/role/list/service/Impl/RoleServiceImpl.java
+++ mall-role-list/src/main/java/com/mall/role/list/service/Impl/RoleServiceImpl.java
@@ -1,13 +1,25 @@
 package com.mall.role.list.service.Impl;
 
+import com.mall.common.pojo.Role;
 import com.mall.role.list.dao.RoleDAO;
 import com.mall.role.list.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
     @Resource
     private RoleDAO roleDAO;
+
+    @Override
+    public List<Role> selectAllRole() {
+        return roleDAO.selectAllRole();
+    }
+
+    @Override
+    public int insertRole(Role role) {
+        return roleDAO.insertRole(role);
+    }
 }
